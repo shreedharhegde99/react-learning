1a6f536af22a7a7e8543c72b6d3294586c5ca70e
Shreedhar Hegde <snhkelemane@gmail.com>
2022-01-08 18:07:04 +0530

    change

--- learn-app/src/App.js
+++ learn-app/src/App.js
@@ -1,20 +1,8 @@
 import "./App.css";
-import EventHandling from "./Components/Events/EventHandling";
-// import List from "./Components/List/List";
-// import ClassExample from "./Components/Classexample";
-// import Stateful, { Stateless } from "./Components/State";
-// import List from "./Components/List";
-// import { Button } from "./Components/Button";
-// import { PaymentCard } from "./Components/Card/PaymentCard";
 
 function App() {
   return (
-    <div style={{ textAlign: "center" }} className="App">
-      Hello World !{/* <ClassExample /> */}
-      {/* <PaymentCard/> */}
-      {/* <List /> */}
-      <EventHandling />
-    </div>
+    
   );
 }
 
--- learn-app/src/Components/Card/Images.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-
-export default  function Images({src}){
-  return (
-    <img src={src} width="30px" alt="icon" />
-  )
-}
