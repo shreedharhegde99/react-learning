445eae5f9711664939e4913f7baf32f58d1bdca7
Shreedhar Hegde <snhkelemane@gmail.com>
2022-01-08 17:44:30 +0530

    change

--- learn-app/src/Redux/learn/actionType.js
+++
@@ -1,4 +0,0 @@
-const Add = "ADD TODO";
-const Toggle = "TOGGLE";
-
-export { Add, Toggle };
--- learn-app/src/Redux/learn/redux.js
+++
@@ -1,7 +0,0 @@
-// action has some type and payload
-
-// actions
-// dispatch
-
-// dispatchers can update state
-// access state by using getState
